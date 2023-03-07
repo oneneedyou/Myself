e6143ea41c988b7e9bd09f2e9fbbd08f80e2fbad
oneneedyou <out1314520ya@outlook.com>
2023-03-07 19:20:19 +0800

    2023年3月9 接口测试，中间版 14:28

--- interface/interface_spring.py
+++ interface/interface_spring.py
@@ -86,27 +86,42 @@ class client_para_:
     port: int = 9881
 
 class client():
+    def print_info(self, des, res, *args, **kwargs):
+        print(f"{des}==============================================")
+        print("状态码: ", res.status_code)
+        print("响应头: ", res.headers)
+        print("响应体: ", res.content.decode(res.apparent_encoding))
+        if res.status_code == 200:
+            return ["success", f"{des} 接口通讯成功"]
+        else:
+            return ["fail", f"{des} 接口通讯失败"]
+
+    def url_(self, para, *args, **kwargs):
+        return f"https://{client_para_.ip}:{client_para_.port}/{para}"
+
+    def planname_to_taskid(self, *args, **kwargs):
+        text = mysql_().MYSQL_1().DB_object(host="192.168.222.202", port=56788, user="Niord", password="hardwork",
+                                            database="docmanager",
+                                            sql_do="select PlanID from plan_info where PlanName='" + str(
+                                                kwargs["plan_name"]) + "';")
+        return text["查询结果数(内容)"][0][0]
+
     def updata_server_Message(self, *args, **kwargs):
         """更新客户端的服务指向"""
         # 客户端更新服务端接口
         methond = "get"
-        url = f"https://{client_para_.ip}:{client_para_.port}/api/system_config/_by_server"
+        url = self.url_("api/system_config/_by_server")
         data = {
             "server_host": kwargs["server_ip"],
             "server_port": kwargs["server_port"],
             "samba_dir": kwargs["server_cache_dir"]
         }
-        res = requests.get(url=url, params=data, headers=None, verify=False)
-        print("更新客户端的服务指向==============================================")
-        print("状态码: ", res.status_code)
-        print("响应头: ", res.headers)
-        print("响应体: ", res.json())
+        return self.print_info(des="更新客户端的服务指向", res=requests.get(url=url, params=data, headers=None, verify=False))
 
     def login(self, *args, **kwargs):
         """获取token, 客户端登录"""
         # 客户端登录接口        #客户端获取token
         client_login_methond = "post"
-        client_login_url = f"https://{client_para_.ip}:{client_para_.port}/api/authenticate"
         client_login_data = {
             "username": client_para_.username,
             "password": md5_().md5(client_para_.password),
@@ -114,122 +129,95 @@ class client():
             "rememberMe": True,
             "isArchive": True
         }
-        res = requests.post(url=client_login_url, json=client_login_data, headers=None, verify=False)
+        res = requests.post(url=self.url_("/api/authenticate"), json=client_login_data, headers=None, verify=False)
         print("获取token, 客户端登录==============================================")
         print("状态码: ", res.status_code)
         print("响应头: ", res.headers)
-        print("响应体: ", res.json())
+        print("响应体: ", res.content.decode(res.apparent_encoding))
         return {"Authorization": "Bearer " + res.json()["id_token"]}
 
     def unlogin(self, *args, **kwargs):
         """客户端登出"""
         # 客户端登出接口
         client_unlogin_methond = "post"
-        client_unlogin_url = f"https://{client_para_.ip}:{client_para_.port}/api/logout"
-        client_unlogin_data = {
+        data = {
             "username": client_para_.username,
             "password": md5_().md5(client_para_.password),
             "verify_code": "code"
         }
-
-        headers = self.login(*args, **kwargs)
-        res = requests.post(url=client_unlogin_url, json=client_unlogin_data, headers=headers, verify=False)
-        print("客户端登出==============================================")
-        print("状态码: ", res.status_code)
-        print("响应头: ", res.headers)
-        print("响应体: ", res.content.decode(res.apparent_encoding))
+        return self.print_info(des="客户端登出", res=requests.post(url=self.url_("api/logout"), json=data, headers=self.login(*args, **kwargs), verify=False))
 
     def set_one_plan(self, *args, **kwargs):
-        """添加计划信息, 客户端建立计划接口"""
+        """添加计划信息, 客户端建立计划接口
+            计划名称、归档问价路径、是否压缩、
+        """
         method = "post"
         planid = str(uuid_().uuid())
-        url = f"https://{client_para_.ip}:{client_para_.port}/api/plan_info"
         data = {
-    "planid": planid,
-    "planname": kwargs["plan_name"],
-    "plandescription": "",
-    "plantype": "0",
-    "plancontent": {
-        "Burn": 0,
-        "CheckPlan": 0,
-        "IsDup": 1,
-        "IsRun": 0,
-        "IsRunAdmin": 1,
-        "IsWithCD": 0,
-        "IsoFormat": 0,
-        "MachineCode": "",
-        "PlanID": planid,
-        "PlanInfo": {
-            "FilePath": [
-                str(kwargs["plan_file_path"])
-            ],
-            "Filter": [],
-            "FilterOut": [],
-            "IsAll": 0,
-            "IsCompress": 0,
-            "IsSetClear": 0,
-            "ClearDays": 0,
-            "PluginDesc": None,
-            "RaidType": None,
-            "RaidNum": 0
-        },
-        "PlanLevel": 0,
-        "PlanName": "千万---文件",
-        "PlanType": 0,
-        "TaskSourceValue": None,
-        "UserID": "",
-        "WriteSytle": 0,
-        "Times": [
-            {
-                "DataType": "单次",
-                "Year": 2023,
-                "Month": 2,
-                "Day": 20,
-                "Hour": "00",
-                "Minute": "00",
-                "Time": "2023-02-19T16:00:00.000Z",
-                "IsAll": True,
-                "TimeName": "",
-                "index": 0
-            }
-        ]
-    },
-    "planstate": 0,
-    "planisonce": 1,
-    "planisexcute": 0,
-    "labels": []
-}
-
-        headers = self.login(*args, **kwargs)
-        res = requests.post(url=url, json=data, headers=headers, verify=False)
-        print("添加计划信息, 客户端建立计划接口==============================================")
-        print("状态码: ", res.status_code)
-        print("响应头: ", res.headers)
-        print("响应体: ", res.content.decode(res.apparent_encoding))
-
-    def execute_plan_instantly(self, *args, **kwargs):
-        """客户端立即执行接口"""
-        text = mysql_().MYSQL_1().DB_object(host="192.168.222.202", port=56788, user="Niord", password="hardwork",
-                                     database="docmanager", sql_do="select PlanID from plan_info where PlanName='" + str(kwargs["plan_name"]) + "';")
-        sql_task_id = text["查询结果数(内容)"][0][0]
-
-        methond = "post"
-        url = f"https://{client_para_.ip}:{client_para_.port}/api/plan_task/immediately"
-        data = {
-            "planid": sql_task_id,
-            "is_all": 1
+            "planid": planid,
+            "planname": kwargs["plan_name"],
+            "plandescription": "",
+            "plantype": "0",
+            "plancontent": {
+                "Burn": 0,
+                "CheckPlan": 0,
+                "IsDup": 1,
+                "IsRun": 0,
+                "IsRunAdmin": 1,
+                "IsWithCD": 0,
+                "IsoFormat": 0,
+                "MachineCode": "",
+                "PlanID": planid,
+                "PlanInfo": {
+                    "FilePath": [
+                        str(kwargs["plan_file_path"])
+                    ],
+                    "Filter": [],
+                    "FilterOut": [],
+                    "IsAll": 0,
+                    "IsCompress": kwargs["is_zip"],
+                    "IsSetClear": 0,
+                    "ClearDays": 0,
+                    "PluginDesc": None,
+                    "RaidType": None,
+                    "RaidNum": 0
+                },
+                "PlanLevel": 0,
+                "PlanName": kwargs["plan_name"],
+                "PlanType": 0,
+                "TaskSourceValue": None,
+                "UserID": "",
+                "WriteSytle": 0,
+                "Times": [
+                    {
+                        "DataType": "单次",
+                        "Year": 2023,
+                        "Month": 2,
+                        "Day": 20,
+                        "Hour": "00",
+                        "Minute": "00",
+                        "Time": "2023-02-19T16:00:00.000Z",
+                        "IsAll": True,
+                        "TimeName": "",
+                        "index": 0
+                    }
+                ]
+            },
+            "planstate": 0,
+            "planisonce": 1,
+            "planisexcute": 0,
+            "labels": []
         }
+        return self.print_info(des="添加计划信息, 客户端建立计划接口", res=requests.post(url=self.url_("api/plan_info"), json=data, headers=self.login(*args, **kwargs), verify=False))
 
-        headers = self.login(*args, **kwargs)
-        res = requests.post(url=url, json=data, headers=headers, verify=False)
-        print("客户端立即执行接口==============================================")
-        print("状态码: ", res.status_code)
-        print("响应头: ", res.headers)
-        # if res.json()["status"] and res.json()["status"] == "INTERNAL_SERVER_ERROR":
-        #     print("ERROR >>> : \n\t", res.json()["message"])
-        # else:
-        #     print("响应体: ", res.content.decode(res.apparent_encoding))
-        print("响应体: ", res.content.decode(res.apparent_encoding))
+    def execute_plan_instantly(self, is_all: int = 1, *args, **kwargs):
+        """客户端立即执行接口"""
+        methond = "post"
+        data = {
+            "planid": self.planname_to_taskid(),
+            "is_all": is_all
+        }
+        return self.print_info(des="客户端立即执行接口", res=requests.post(url=self.url_("api/plan_task/immediately"), json=data, headers=self.login(*args, **kwargs), verify=False))
 
     def mysql_test_link(self):
         pass
